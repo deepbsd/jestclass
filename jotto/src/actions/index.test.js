ae328fd3ac7d64130758088818cc62de95219c4b
David S. Jackson <deepbsd@yahoo.com>
2018-07-02 14:13:21 -0400

    created actions file and test for successReducer

---
+++ jotto/src/actions/index.test.js
@@ -0,0 +1,16 @@
+import {correctGuess, actionTypes } from './';
+
+
+describe('correctGuess()', () => {
+
+    it('returns an object with type `CORRECT_GUESS`', () => {
+      const action = correctGuess();
+      expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
+    });
+
+
+
+
+})
+
+
