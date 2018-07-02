af3b0a24f4bf1c17fb52c68070dea71bf8a6f01a
David S. Jackson <deepbsd@yahoo.com>
2018-07-02 04:06:41 -0400

    added reducer thunk store redux

---
+++ jotto/src/reducers/successReducer.js
@@ -0,0 +1,10 @@
+/**
+ * @function success reducer
+ * @param {array} state - Array of guessed words
+ * @param {object} action - action to be reduced
+ * @array {boolean} - new success state
+ */
+
+export default (state, action) => {
+  return null;
+}
